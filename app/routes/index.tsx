2667d6e13d49da51401d63a7fcd399e1095eeb4a
Heliton Nordt <1625399+hnordt@users.noreply.github.com>
2022-09-19 18:07:38 -0300

    Minor change

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,7 @@
-const IndexRoute = () => <h1>It works!</h1>
+const IndexRoute = () => (
+  <div className="mx-auto my-12 w-full max-w-xl">
+    <h1>It works!</h1>
+  </div>
+)
 
 export default IndexRoute
