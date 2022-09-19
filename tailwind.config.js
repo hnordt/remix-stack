7376997ceaad2133e552e6724c476ded0e99b141
Heliton Nordt <1625399+hnordt@users.noreply.github.com>
2022-09-19 18:05:27 -0300

    Add Tailwind plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{ts,tsx}"],
+  content: ["app/**/*.{ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/line-clamp"),
+    require("tailwindcss-radix")(),
+  ],
 }
