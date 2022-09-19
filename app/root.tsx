365046b306effa1172c330ca12f33ec4ab350608
Heliton Nordt <1625399+hnordt@users.noreply.github.com>
2022-09-19 18:02:09 -0300

    Add Tailwind

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import type { MetaFunction } from "@remix-run/node"
+import type { LinksFunction, MetaFunction } from "@remix-run/node"
 import {
   Links,
   LiveReload,
@@ -7,12 +7,15 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react"
+import styles from "~/styles/index.dist.css"
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "Remix Stack",
   viewport: "width=device-width, initial-scale=1",
 })
+
+export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }]
 
 const App = () => (
   <html lang="en">
